731b7f86d2e6689da58d7013a2ee9857f141c2a0
scotty1246 <scotty1246@users.noreply.github.com>
2017-04-05 11:17:31 -0400

    Added writebinary.cpp

---
+++ writebinary.cpp
@@ -0,0 +1,26 @@
+#include<fstream>
+#include <iostream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+ 
+   ofstream out{"abc.bin"};
+   if(out)
+   {
+    out << a << " " << b << " " << c; 
+   }
+   else
+   {
+      cout << "Error opening file";
+   }   
+//cout<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
+   
+   cout<<endl<<endl;
+ 
+   return 0;
+}
